281713a0389432fcf4fc83a55f3b03741451819f
gantanikhilraj <gantanikhilraj@gmail.com>
2023-09-23 12:42:44 +0530

    prime

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ const NumberManagement = () => {
 
   const handleFetchNumbers = async () => {
     try {
-      const response = await axios.get(`/api/numbers?url=${urls}`);
+      const response = await axios.get(`http://20.244.56.144/numbers/primes`);
       setMergedNumbers(response.data.numbers);
     } catch (error) {
       console.error('Error fetching numbers:', error);
